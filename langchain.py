6c394a218369cf71b58a70b7a0c5a6667f61c290
Shashank <shashank21005@gmail.com>
2023-09-19 19:17:47 +0530

    added some workflow to be followed for langchain model

---
+++ langchain.py
@@ -0,0 +1,10 @@
+'''
+1. getting all the details of the user's complaint
+2. using that complaint to map to the best suitable government employee
+    a. initial complaint -> llm routing -> trying to get the best fitted \
+    employees list -> vector database + langchain query
+    b. giving the user this freedom to select any one of the filtered employees \
+    or we may  even automate this as using  Map re-rank concept available in langchain docs\
+    or any other better way  
+3. then re-processing the complaint and connecting it with the employee
+'''
