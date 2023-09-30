84eef9b054bd09e8048e2aa1590c785a10412e95
Aditya Pratap Singh <adityapratapsingh12b05@gmail.com>
2023-09-30 11:00:25 +0530

    modified

--- django_chatbot/chatbot/urls.py
+++ django_chatbot/chatbot/urls.py
@@ -2,14 +2,12 @@ from django.urls import path
 from .import views
 
 urlpatterns=[
-    # path('', views.register, name='register'),
+    path('', views.register, name='register'),
     path('login/', views.login, name='login'),
-    path('',views.chatbot,name='chatbot'),
+    path('chatbot/',views.chatbot,name='chatbot'),
     path('logout/', views.logout, name='logout'),
     path('user_dash/', views.user_dash, name='dashboard'),
     path('profile/', views.profile, name='profile'), 
     # path('home/', views.home, name='home'),
     # path('user_dash/', views.user_dash, name='dashboard'),
-
-
 ]
