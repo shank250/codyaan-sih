3dcbe58364843ca55034050a64597a8259c5b0d2
Aditya Pratap Singh <adityapratapsingh12b05@gmail.com>
2023-11-29 19:46:14 +0530

    updated

--- django_chatbot/chatbot/views.py
+++ django_chatbot/chatbot/views.py
@@ -1,41 +1,36 @@
+import os
+import json
+import openai
+import chromadb
+import pymongo
 
 from django.contrib.auth import get_user_model
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
-import openai
 from django.contrib import auth
-from django.contrib.auth.models import User
-# from .models import Chat
-from django.utils import timezone
-import os
-from langchain.memory import ChatMessageHistory
-import pymongo
+from .models import UserProfile
 
-from langchain.memory import ConversationSummaryBufferMemory
-from langchain.chat_models import ChatOpenAI
+from pymongo import MongoClient
+
+from langchain.memory import ChatMessageHistory
 from langchain.prompts import ChatPromptTemplate
+from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
-import json
+
 chat_summary = ""
 chat_summary_status = False
-
 os.environ['OPENAI_API_KEY'] = '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'
-
-
 history = ChatMessageHistory()
 user_chat = []
 ai_chat = []
-email = None
-
-# Create your views here.
-
 openai_api_key = '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'
 openai.api_key = openai_api_key
+llm = ChatOpenAI(temperature=0.6)
+
 
 def complaint_mongodb():
-    llm = ChatOpenAI(temperature=0.6)
     prompt = ChatPromptTemplate.from_template(
         """You are banking grivance complaint loger. 
         
@@ -64,44 +59,25 @@ def complaint_mongodb():
     dictionary_response = json.loads(complaint_dict_str)
     dictionary_response["employeeId"] = "rajesh.yadav@gmail.com"
     print(dictionary_response)
-    import pymongo
-    from pymongo import MongoClient
+    
     connection_string = "mongodb+srv://codyaanSIH:VEkE6wjohQ9v01De@cluster0.vkfifhe.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp"
-
-    # Replace these with your MongoDB Atlas connection details
-    # connection_string = ""
-
-    # Initialize the MongoDB client
     client = MongoClient(connection_string)
-
-    # Select the database you want to use
     db = client['test']
-
-    # Select the collection within the database
     collection = db['complaints']
 
-    # Data to insert into the collection
     data_to_insert = dictionary_response
-# tyoe refers catagory
     # Insert the data into the collection
     insert_result = collection.insert_one(data_to_insert)
 
-    # Check if the insertion was successful
     if insert_result.acknowledged:
         print("Data inserted successfully. Inserted ID:", insert_result.inserted_id)
     else:
         print("Data insertion failed.")
-
-    # Close the MongoDB connection
     client.close()
 
 
-# now giving my chat description of the llm for vector search
 def vector_search(chat_summary = chat_summary, user_chat = user_chat):
-    import pymongo
-    import json
-    import time
-    llm = ChatOpenAI(temperature=0.6)
+    
 
     prompt = ChatPromptTemplate.from_template(
         """You are a grivance complaint registration bot\
@@ -110,23 +86,15 @@ def vector_search(chat_summary = chat_summary, user_chat = user_chat):
         vector search should contain bank name, type of the problem, problem catagory, languge in which user is talking location where the complaint belongs to. \
             here is the user detailed problem  : {summary}"""
     )
-
     summary = str(user_chat) + chat_summary
     print(summary)
     chain = LLMChain(llm=llm, prompt=prompt)
     vector_search_query = chain.run(summary)
     print(vector_search_query)
   
-
-    start_time = time.time()
-    # Replace these with your MongoDB Atlas connection details
     mongo_uri = "mongodb+srv://codyaanSIH:VEkE6wjohQ9v01De@cluster0.vkfifhe.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp"
     collection_name = "employees"
-
-    # Connect to MongoDB Atlas
     client = pymongo.MongoClient(mongo_uri)
-
-    # Access the database and collection
     db = client.get_database("test")
     collection = db[collection_name]
 
@@ -139,7 +107,6 @@ def vector_search(chat_summary = chat_summary, user_chat = user_chat):
     ids = []
     # Iterate over the results and print them
     for document in result:
-        # print(document)
         lang = str(document["language"])
         comp_specialisation = str(document["complaintSpecialisation"])
         emp_description = document["bankName"] + document["workplace"] + lang + comp_specialisation
@@ -151,15 +118,7 @@ def vector_search(chat_summary = chat_summary, user_chat = user_chat):
         meta_dict = json.loads(meta_string)
         metadatas.append(meta_dict)
         ids.append(emp_ids)
-    # Close the MongoDB connection
     client.close()
-    # print(documents)
-    # print(metadatas)
-    # print(ids)
-
-
-
-    import chromadb
 
     chroma_client = chromadb.Client()
     collection = chroma_client.create_collection(name="my_bank_col")
@@ -169,7 +128,6 @@ def vector_search(chat_summary = chat_summary, user_chat = user_chat):
         metadatas=metadatas,
         ids=ids
     )
-
     results = collection.query(
         query_texts=[vector_search_query],
         n_results=1
@@ -178,23 +136,6 @@ def vector_search(chat_summary = chat_summary, user_chat = user_chat):
     complaint_mongodb()
     return vector_search_query
 
-
-
-
-def ask_openai(message):
-    response = openai.ChatCompletion.create(
-        model = "gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "You are an helpful assistant."},
-            {"role": "user", "content": message},
-        ]
-    )
-    print(response)
-    answer = response.choices[0].message.content.strip()
-    return answer
-
-
-from pymongo import MongoClient
 
 def login(request):
     if request.method == 'POST':
@@ -232,10 +173,6 @@ def login(request):
     return render(request, 'login.html') 
 
 
-from django.contrib.auth.decorators import login_required
-from .models import UserProfile
-import json
-
 def profile(request):
     emailID = request.session.get('emailID', None)
     client = MongoClient('mongodb://localhost:27017/')
@@ -256,51 +193,25 @@ def profile(request):
 
 
 def chatbot(request):
-    # redirect('chatbot')
-    print("entering chatbot")
-    # return render(request, 'chatbot.html')
-    # chats = Chat.objects.filter(user=request.user)
-    # complaint_mongodb()
-    # global emailID
-    # file_path = 'email.txt'
     emailID = request.session.get('emailID', None)
-    # if emailID:
-    #     with open(file_path, 'w') as txt:
-    #         txt.write(emailID)
-    # with open(file_path, 'r') as txt:
-    #     emailID = txt.read().strip() 
-    # email= request.GET.get('email', 'No email provided') 
     client = MongoClient('mongodb://localhost:27017/')
     db = client['User_database']  
     users_collection = db['chatbot_userprofile']
-    print('mongdb connected')
     print(emailID)
     user_document = users_collection.find_one({'email': emailID})
-    print(type(user_document))
     print(user_document)
-    print( user_document['full_name'])
-    # user_data_str=user_document['username']
-    # user_data_dict=user_data_str.replace('\'','\"')
-    # username_json = json.loads(user_data_dict)
-    # print(username_json)
+    # print( user_document['full_name'])
     context = {
         'user_profile': user_document['full_name'],
-    }
+    }   
     
-
     if request.method == 'POST':
         user_query = request.POST.get('message')
         user_chat.append(user_query)
         history.add_user_message(user_query)
 
         global chat_summary
-        from langchain.chat_models import ChatOpenAI
-        from langchain.prompts import ChatPromptTemplate
-        from langchain.chains import LLMChain
-        import os
-
         os.environ['OPENAI_API_KEY'] = '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'
-
         llm = ChatOpenAI(temperature=0.6)
         prompt = ChatPromptTemplate.from_template(
             """You are banking grivance complaint loger. 
@@ -336,10 +247,6 @@ def chatbot(request):
         print(response)
 
         # creating a chat summary
-        from langchain.chat_models import ChatOpenAI
-        from langchain.prompts import ChatPromptTemplate
-        from langchain.chains import LLMChain
-        llm = ChatOpenAI(temperature=0.6)
         prompt = ChatPromptTemplate.from_template(
             """Make a chat summary : \
             keeping all the facts correct and without missing any important information \
@@ -353,12 +260,7 @@ def chatbot(request):
         chain = LLMChain(llm=llm, prompt=prompt)
         chat =  chat_summary + user_chat[-1] + ai_chat[-1]
         chat_summary = chain.run(chat)
-        
-        # response = ask_openai(message)
-        # response = "hi this my response."
-        # print(response)
-        # chat = Chat(user=request.user, message=message, response=response, created_at=timezone.now())
-        # chat.save()
+
         if response == "[COMPLAINT-FILABLE]":
             status = "done"
             print("moving it to create a vector search prompt")
@@ -367,28 +269,55 @@ def chatbot(request):
         print("\n oopes trying to get more data")
         status = "more-data-req"
         print(response)
-        # dictionary_response = json.loads(response)
-        # response_ai = dictionary_response["chat-reply"]
-        # user_query = input(dictionary_response["chat-reply"] + "\nuser : ")
         print("CHAT SUMmARY : " + chat_summary)
-        # complaint_completion(str(history.messages) + user_query)
         return JsonResponse({'message': user_query, 'response': response})
-    
-    
-
-    # elif response == "[IRRELEVANT]":
-    #     print("not sure what you are talking about")
-    #     status = "new-chat"
-    # else:
-    #     print("\n oopes trying to get more data")
-    #     status = "more-data-req"
-    #     # print(response)
-    #     dictionary_response = json.loads(response)
-    #     user_query = input(dictionary_response["chat-reply"] + "\nuser : ")
-    #     print("CHAT SUMmARY : " + chat_summary)
-    #     complaint_completion(str(history.messages) + user_query)
-
     return render(request, 'chatbot.html', context)
+
+
+def register(request):
+    if request.method == 'POST':
+        full_name = request.POST['username']
+        public_id = request.POST['userID']
+        mobile_number = request.POST['number']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']        
+        aadhar_number = request.POST['aadharNo']
+        language = request.POST['language']
+
+        user = get_user_model().objects.create(username=full_name, password=password1)
+
+        if password1 == password2:
+            user_profile = UserProfile(
+                user=user,
+                full_name=full_name,
+                public_id=public_id,
+                mobile_number=mobile_number,
+                email=email,
+                password=password1,
+                aadhar_number=aadhar_number,
+                language=language
+            )
+            user_profile.save()
+            # auth.login(request, user)
+            request.session['emailID'] = email
+            return redirect('chatbot') 
+    return render(request, 'register.html')
+
+
+def home(request):
+    return render(request,'home.html')
+
+
+def user_dash(request):
+    template = loader.get_template('user_dash.html')
+    return HttpResponse(template.render())
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('login')
+
 
 
 
@@ -440,55 +369,3 @@ def chatbot(request):
 #             auth.login(request, user)
 #             return redirect('chatbot')
 #     return render(request, 'register.html')
-
-from django.shortcuts import render, redirect
-from .models import UserProfile
-
-def register(request):
-    if request.method == 'POST':
-        full_name = request.POST['username']
-        public_id = request.POST['userID']
-        mobile_number = request.POST['number']
-        email = request.POST['email']
-        password1 = request.POST['password1']
-        password2 = request.POST['password2']        
-        aadhar_number = request.POST['aadharNo']
-        language = request.POST['language']
-
-        # Create and save a new UserProfile object
-        user = get_user_model().objects.create(username=full_name, password=password1)
-
-        if password1 == password2:
-           
-            user_profile = UserProfile(
-                user=user,
-                full_name=full_name,
-                public_id=public_id,
-                mobile_number=mobile_number,
-                email=email,
-                password=password1,
-                aadhar_number=aadhar_number,
-                language=language
-            )
-            user_profile.save()
-            # auth.login(request, user)
-            request.session['emailID'] = email
-
-            return redirect('chatbot') 
-    return render(request, 'register.html')
-
-
-def home(request):
-    return render(request,'home.html')
-
-
-
-def user_dash(request):
-    template = loader.get_template('user_dash.html')
-    return HttpResponse(template.render())
-
-
-
-def logout(request):
-    auth.logout(request)
-    return redirect('login')
